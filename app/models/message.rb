9c751fdb21127931f326d4e0b261e84d71a0bb0a
8ENs <ben@coderush.ca>
2015-05-18 01:01:22 +0000

    Add URL working

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,12 @@
 class Message < ActiveRecord::Base
   validates :title, :content, :author, presence: true
+
+  validate  :url_was_not_legit
+
+  def url_was_not_legit
+    # don't throw an error unless it's a valid url (regex) OR the url was empty
+    unless /^(https?:\/\/[a-zA-Z|\d]{2,}\.[a-zA-Z|\d|\.]{2,})/.match(url) || url == ""
+      errors.add(:url_was_not_legit, "--- your URL input was not legit! (please try again and include 'http://')")
+    end
+  end
 end
